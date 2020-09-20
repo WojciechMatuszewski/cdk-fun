666d25bf42cf896223504c5981ca2e21cb8909c4
Wojciech Matuszewski <wm.matuszewski@wp.pl>
2020-09-20 13:15:05 +0200

    just return

--- backend/functions/auto-confirm-email.ts
+++ backend/functions/auto-confirm-email.ts
@@ -1,10 +1,11 @@
 import { PreSignUpTriggerHandler } from "aws-lambda";
 
-const handler: PreSignUpTriggerHandler = (event, ctx, callback) => {
-  event.response.autoConfirmUser = true;
-  console.log("about to return", JSON.stringify(event, null, 2));
+const handler: PreSignUpTriggerHandler = async event => {
+  // event.response.autoConfirmUser = true;
+  // console.log("about to return", JSON.stringify(event, null, 2));
 
-  return callback(null, event);
+  return event;
+  // return callback(null, event);
 };
 
 export { handler };
